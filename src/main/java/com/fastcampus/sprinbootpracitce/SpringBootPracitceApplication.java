c0a684a6fdcbc47bcdd0b4f28b7e628642a92bad
CapitalSnob <sorkejso@gmail.com>
2023-12-07 08:17:23 +0900

    Initialize #1 - spring boot
    스프링 부트 셋업 완료

---
+++ src/main/java/com/fastcampus/sprinbootpracitce/SpringBootPracitceApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.sprinbootpracitce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPracitceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPracitceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fastcampus/sprinbootpracitce/SpringBootPracitceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.sprinbootpracitce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPracitceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
